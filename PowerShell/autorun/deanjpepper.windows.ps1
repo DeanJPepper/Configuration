4de920beffa0d638fc0192d4247ce0a885d84127
Dean Pepper <DPepper@phlexglobal.com>
2020-02-24 10:55:37 +0000

    Added PowerShell functions for base-64 encode and decode.

--- PowerShell/autorun/deanjpepper.windows.ps1
+++ PowerShell/autorun/deanjpepper.windows.ps1
@@ -1,8 +1,10 @@
+# Open new instance of PowerShell
 function newps 
 {
 	start powershell
 }
 
+# Open Windows Explorer at current/specified directory
 function ex
 {
 	if ([String]::IsNullOrWhiteSpace($args[0]))
@@ -15,13 +17,27 @@ function ex
 	}
 }
 
-function npp
-{
-	& 'C:\Program Files (x86)\Notepad++\notepad++.exe' $args[0]
-}
-
+# Create a directory and change into it
 function mkdircd
 {
 	mkdir $args[0]
 	cd $args[0]
+}
+
+# Base 64 encode string
+function base64encode
+{
+	Write-Host ([System.Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($args[0])))
+}
+
+# Base 64 decode string
+function base64decode
+{
+	Write-Host ([Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($args[0])))
+}
+
+# Open specified file in Notepad++
+function npp
+{
+	& 'C:\Program Files (x86)\Notepad++\notepad++.exe' $args[0]
 }
