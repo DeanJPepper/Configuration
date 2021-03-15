6d90d8d5d3faddd597d1af00e1ea88fc6e084cb3
Dean Pepper <deanjpepper@gmail.com>
2021-03-15 18:38:12 +0000

    Updated PowerShell prompt.

--- PowerShell/deanjpepper.prompt.psm1
+++ PowerShell/deanjpepper.prompt.psm1
@@ -1,21 +1,38 @@
-# Change the prompt to show the Git status or the current path
-function prompt {
+function promptDirectory {
 	$currentLoction = $ExecutionContext.SessionState.Path.CurrentLocation
 	$host.UI.RawUi.WindowTitle = $currentLoction
-	Write-Host "$env:UserName@$env:ComputerName " -NoNewLine -ForegroundColor DarkGray
-	
-	$isGitInstalled = Get-Command -ErrorAction SilentlyContinue git
+
+	Write-Host "dir:" -NoNewline -ForegroundColor DarkGray
+	Write-Host $currentLoction -ForegroundColor Yellow
+}
+
+function promptGit {
+	$isGitInstalled = Get-Command -ErrorAction SilentlyContinue Get-IsGitRepository
+
 	if ($isGitInstalled -and $(Get-IsGitRepository)) {
+		Write-Host "git:" -NoNewline -ForegroundColor DarkGray
 		Show-GitSummary
-	} elseif ($env:KubeConfig) {
-		if (Test-Path $env:KubeConfig) {
-			Write-Host $([System.IO.Path]::GetFileName($env:KubeConfig)) -NoNewLine -ForegroundColor Yellow
-		} else {
-			Write-Host $env:KubeConfig -NoNewLine -ForegroundColor Red
-		}
-	} else {
-		Write-Host $currentLoction -NoNewLine -ForegroundColor Yellow
+		Write-Host
 	}
-	
+}
+
+function promptKubernetes {
+	$configFile = $env:KubeConfig
+
+	if ($configFile) {
+		Write-Host "k8s:" -NoNewline -ForegroundColor DarkGray
+		if (Test-Path $configFile) {
+			Write-Host $([System.IO.Path]::GetFileName($configFile)) -ForegroundColor Yellow
+		} else {
+			Write-Host $configFile -NoNewLine -ForegroundColor Red
+		}
+	}
+}
+
+function prompt {	
+	Write-Host
+	promptDirectory
+	promptGit
+	promptKubernetes		
 	return "> "
 }
